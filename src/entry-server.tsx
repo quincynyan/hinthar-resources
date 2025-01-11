662dc516a152d0315d2e5b7e45f59abebdb06dbd
Quincy Nyan <quincynyan@gmail.com>
2025-01-11 20:41:02 +0630

    revert everything

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -12,7 +12,6 @@ export default createHandler(() => (
 						content="width=device-width, initial-scale=1"
 					/>
 					<link rel="icon" href="/favicon.ico" />
-					<base href="https://hinthar.education/resources" />
 					{assets}
 				</head>
 				<body>
