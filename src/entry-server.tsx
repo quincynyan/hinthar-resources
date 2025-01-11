6c219e2c6883d1fcf7d66dc1b887609ae5e39d86
Quincy Nyan <quincynyan@gmail.com>
2025-01-11 20:27:37 +0630

    bruh typo fix

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -12,7 +12,7 @@ export default createHandler(() => (
 						content="width=device-width, initial-scale=1"
 					/>
 					<link rel="icon" href="/favicon.ico" />
-					<base href="/blog/" />
+					<base href="/resources/" />
 					{assets}
 				</head>
 				<body>
