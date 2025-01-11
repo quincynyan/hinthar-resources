bde3896b317d300567d4cae9c9f467c1f5c60656
Quincy Nyan <quincynyan@gmail.com>
2025-01-11 20:22:52 +0630

    Add base

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -12,6 +12,7 @@ export default createHandler(() => (
 						content="width=device-width, initial-scale=1"
 					/>
 					<link rel="icon" href="/favicon.ico" />
+					<base href="/blog/" />
 					{assets}
 				</head>
 				<body>
